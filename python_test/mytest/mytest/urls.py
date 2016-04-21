621316a577e86961443c0aeb69dc927e066fb302
orange <orange@server>
2016-04-21 14:31:12 +0800

    Thu Apr 21 14:31:12 CST 2016 from test001.com

--- python_test/mytest/mytest/urls.py
+++ python_test/mytest/mytest/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-from mytest.views import  hello, my_homepage_view, current_datetime, hour_ahead
+from mytest.views import  hello, my_homepage_view, current_datetime, hour_ahead, book_list
+
 
 
 urlpatterns = patterns('',
@@ -7,5 +8,6 @@ urlpatterns = patterns('',
     ('^$', my_homepage_view),
     ('^time/$', current_datetime),
     (r'^time/plus/(\d{1,2}?)/$', hour_ahead),
+    ('^booklist/$', book_list),
     #...
 )
--- python_test/mytest/mytest/views.py
+++ python_test/mytest/mytest/views.py
@@ -3,6 +3,7 @@
 #from django.http import HttpResponse
 from django.shortcuts import render_to_response
 import datetime
+import MySQLdb
 def hello(request):
     return HttpResponse('Hello World Hi')
 
@@ -10,8 +11,14 @@ def my_homepage_view(request):
     return HttpResponse('home')
     
 def current_datetime(request):
-    now = datetime.datetime.now()
-    return render_to_response('current_datetime.html', {'current_date': now})
+    #now = datetime.datetime.now()
+    #return render_to_response('current_datetime.html', {'current_date': now})
+
+    current_date = datetime.datetime.now()
+    current_section = "op"
+    i = range(10)
+    #print i
+    return render_to_response('current_datetime.html', locals())
     #t = get_template('current_datetime.html')
 #    t = Template("<html><body> It is now {{current_date}}.</body></html>")
     #html = t.render(Context({'current_date': now}))
@@ -24,7 +31,18 @@ def hour_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    return render_to_response('hours_ahead.html', locals())
 #    assert False
-    html = "<html><body>In %s hour(s), it will be %s.</html></body>" % (offset, dt)
-    return HttpResponse(html)
+    #html = "<html><body>In %s hour(s), it will be %s.</html></body>" % (offset, dt)
+    #html = "<html><body>In %s hour(s), it will be %s.</html></body>" % (offset, dt)
+    #return HttpResponse(html)
     
+
+def book_list(request):
+    #db = MySQLdb.connect(user='root', db='orange', passwd='rootroot', host='localhost', unix_socket='/tmp/mysql.sock')
+    db = MySQLdb.connect(user='root', db='orange', passwd='rootroot', host='127.0.0.1')
+    cursor = db.cursor()
+    cursor.execute('select * from test002')
+    names = [row[0] for row in cursor.fetchall()]
+    db.close()
+    return render_to_response('book_list.html', {'names': names})
